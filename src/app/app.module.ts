907f26141395510b037ebb53acee3fc1e5cdd398
Fahrettin UYGUN <fahrettin@Fahrettins-MacBook-Pro.local>
2020-05-26 00:40:54 +0300

    maked routing settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MasterComponent } from './master/master.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
-import { ContentComponent } from './content/content.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,8 @@ import { ContentComponent } from './content/content.component';
     HeaderComponent,
     FooterComponent,
     MenuComponent,
-    ContentComponent
+    routingComponents,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
